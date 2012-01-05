0fc756b88d2c396b7b15689b9e529d9013f3e0b3
ggodoy <ggodoy@pampacomcdt.com.ar>
2012-01-05 17:10:48 -0300

    #14 Plugin social Facebook Me gusta

--- header.php
+++ header.php
@@ -54,6 +54,14 @@
 </head>
 
 <body <?php body_class(); ?>>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <div id="wrapper" class="hfeed">
 	<div id="header">
 
